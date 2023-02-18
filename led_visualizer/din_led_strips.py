ccc87f3c891dc50a0c4cf6dfa95b9a5a3a2606f1
matthiash <matthias.huser@adfinis.com>
2023-02-18 21:39:15 +0100

    working

--- led_visualizer/din_led_strips.py
+++ led_visualizer/din_led_strips.py
@@ -11,14 +11,13 @@ import signal
 import time
 import readchar
 
-
 ### Configuration ###
 
 MAX_GREEN = 140
 MAX_ORANGE = 210
 
 NUMBER_OF_PIXELS_PER_BAR = 60
-NUMBER_OF_BARS = 5
+NUMBER_OF_BARS = 6
 
 BRIGHTNESS = 0.1
 
@@ -34,26 +33,32 @@ RAW_TARGET = "/dev/stdout"
 conpat = """
 [general]
 bars = %d
+framerate = 30
 [output]
 method = raw
 raw_target = %s
 bit_format = %s
 channels = mono
 [smoothing]
-noise_reduction = 0
+noise_reduction = 30
 [eq]
 1 = 1
-2 = 0.9
-3 = 0.9
-4 = 0.9
+2 = 1
 """
 
 MAX_PIXEL = (NUMBER_OF_PIXELS_PER_BAR*NUMBER_OF_BARS)
 
 PIXEL_COM = neopixel.NeoPixel(board.D18, MAX_PIXEL, auto_write=False, pixel_order=neopixel.RGB, brightness=BRIGHTNESS)
 
+OUTPUT = ""
+
 config = conpat % (NUMBER_OF_BARS, RAW_TARGET, OUTPUT_BIT_FORMAT)
 bytetype, bytesize, bytenorm = ("H", 2, 65535) if OUTPUT_BIT_FORMAT == "16bit" else ("B", 1, 255)
+
+# print color
+#def color_encode(pixel):
+#    return f"\033[38;2;{pixel[0]};{pixel[1]};{pixel[2]}m*\033[0m"
+
 
 def exit(signum, frame):
     PIXEL_COM.fill((0,0,0))
@@ -63,35 +68,34 @@ def exit(signum, frame):
 
 signal.signal(signal.SIGINT, exit)
 
+def cache_pixels():
+    cache = {}
+    for number in range(1+(NUMBER_OF_PIXELS_PER_BAR*NUMBER_OF_BARS)):
+        current_bar = number // NUMBER_OF_PIXELS_PER_BAR 
+        if current_bar % 2 == 1 and current_bar != 0:
+            real_number = abs(number - ((NUMBER_OF_PIXELS_PER_BAR-1) + current_bar*NUMBER_OF_PIXELS_PER_BAR))+current_bar*NUMBER_OF_PIXELS_PER_BAR
+            cache[number] = real_number
+        else:
+            cache[number] = number
+    return cache
+
+cache = cache_pixels()
+
 def set_pixel(PIXEL_COM, number, value):
-    # Teilnung ohne rest
-    current_bar = number // NUMBER_OF_PIXELS_PER_BAR 
-    # If current bar is odd or 0
-    if current_bar % 2 == 1 and current_bar != 0:
-        # Invert the pixels!
-        real_number = abs(number - ((NUMBER_OF_PIXELS_PER_BAR-1) + current_bar*NUMBER_OF_PIXELS_PER_BAR))+current_bar*NUMBER_OF_PIXELS_PER_BAR
-        # SETZNE!!
-        PIXEL_COM[real_number] = value
-    else:
-        PIXEL_COM[number] = value
+    PIXEL_COM[cache[number]] = value
 
 def calc(calc_in):
-    calc_out = (((NUMBER_OF_PIXELS_PER_BAR-1)/255)*calc_in)
-    calc_out = int(calc_out)
+    calc_out = int(((NUMBER_OF_PIXELS_PER_BAR-1)/255)*calc_in)
     return calc_out
 
 def pixel_define(pd_sample, pd_count_bar, pd_low, pd_max):
-    pd_set_pixel = calc(pd_low)
-    pd_set_pixel = pd_set_pixel+((pd_count_bar-1)*NUMBER_OF_PIXELS_PER_BAR)
-    pd_calc_sample = calc(pd_sample)
-    pd_calc_sample = (pd_calc_sample+((pd_count_bar-1)*NUMBER_OF_PIXELS_PER_BAR))
-    pd_end_pixel = calc(pd_max-1)
-    pd_end_pixel = pd_end_pixel+((pd_count_bar-1)*NUMBER_OF_PIXELS_PER_BAR)
+    pd_set_pixel = calc(pd_low)+((pd_count_bar-1)*NUMBER_OF_PIXELS_PER_BAR)
+    pd_calc_sample = calc(pd_sample)+((pd_count_bar-1)*NUMBER_OF_PIXELS_PER_BAR)
+    pd_end_pixel = calc(pd_max-1)+((pd_count_bar-1)*NUMBER_OF_PIXELS_PER_BAR)
     return pd_set_pixel, pd_calc_sample, pd_end_pixel
 
 def set_dark(sd_sample, sd_count_bar):
-    sd_calc_sample = calc(sd_sample)
-    sd_calc_sample = (sd_calc_sample+((sd_count_bar-1)*NUMBER_OF_PIXELS_PER_BAR))
+    sd_calc_sample = calc(sd_sample)+((sd_count_bar-1)*NUMBER_OF_PIXELS_PER_BAR)
     sd_max_pixel = ((sd_count_bar*NUMBER_OF_PIXELS_PER_BAR)-1)
     while sd_max_pixel > sd_calc_sample:
         set_pixel(PIXEL_COM, sd_max_pixel, (0, 0, 0))
@@ -116,7 +120,7 @@ def set_red(r_sample, r_count_bar):
         r_set_pixel += 1
 
 def band_split(d_sample, d_count_bar):
-    print(d_sample)
+    #print(d_sample)
     set_dark(d_sample, d_count_bar)
     if d_sample < MAX_GREEN:
         set_green(d_sample, d_count_bar)
@@ -146,20 +150,21 @@ def run():
             source = open(RAW_TARGET, "rb")
         else:
             source = process.stdout
-  
-        while True:
+
+        while 1:
             data = source.read(chunk)
             if len(data) < chunk:
                 break
             sample = struct.unpack(fmt, data)
 
             count_bar = 1
-            print("---------------")
             while count_bar <= NUMBER_OF_BARS:
-                print("BAR[" + str(count_bar) + "]: " , end = '')
+                #print('\033[?25l', end="")
+                #print("BAR[" + str(count_bar) + "]: ", end = '')
                 band_split(int(sample[(count_bar-1)]), count_bar)
                 count_bar += 1
             PIXEL_COM.show()
+            #print("\033c", end="")
   
 if __name__ == "__main__":
     run()
